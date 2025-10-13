945856ab8d7eb4d15157e83e9dad50e9a88d7f96
Abdulselam Taye <abdulselam.taye-ug@aau.edu.et>
2025-10-13 22:36:07 +0300

    add navbar from shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import NavBar from "./components/ui/navbar/NavBar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-400 to-purple-600 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-xl text-center">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Murajea Web-app
-        </h1>
-        <p className="text-gray-600 mb-6">Tailwind CSS is working!</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg transition duration-200">
-          Test Button
-        </button>
-      </div>
+    <div>
+      <NavBar />
     </div>
   );
 }
